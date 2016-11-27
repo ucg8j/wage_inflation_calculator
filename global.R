c414672412699b8eb6749712e1f5e668c021d749
ucg8j <lukesingham@gmail.com>
2016-11-27 20:16:25 +1100

    function to adjust wages up or down

--- global.R
+++ global.R
@@ -7,3 +7,16 @@ rates <- read_csv('cpi.csv')
 
 # what's the avg of the 3 years?
 avg.cpi <- mean(rates$rate)
+
+# return vector of adjusted wages
+adj_wage <- function(wage, rates, inflate){
+
+    wages <- vector()
+    
+    for (i in rates){
+        w       <- ifelse(inflate, wage - wage*i, wage + wage*i)
+        wages   <- c(wages,w)
+    }
+    
+    return(wages)
+}
