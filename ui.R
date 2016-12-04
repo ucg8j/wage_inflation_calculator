4edea274120caf71b2491932b4c718853555221e
ucg8j <lukesingham@gmail.com>
2016-12-04 12:30:08 +1100

    add google analytics

--- ui.R
+++ ui.R
@@ -1,13 +1,18 @@
 fluidPage(
+    # include google analytics
+    tags$head(includeScript("google-analytics.js")),
+    
     # Application title
     titlePanel("Are your wages keeping up?"),
     
+    # User Input
     sidebarLayout(
         sidebarPanel(
             numericInput("wage", label = "Enter your wage", value = 75000),
             helpText('Source: RBA.gov.au')
         ),
     
+    # Outputs
     mainPanel(
         plotlyOutput("main_plot"),
         textOutput('inflate_txt'), br(),
