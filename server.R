d951e0d7d1b7ff1e41138361145ec980bcef8793
ucg8j <lukesingham@gmail.com>
2016-12-04 12:07:04 +1100

    small tidy

--- server.R
+++ server.R
@@ -11,18 +11,20 @@ shinyServer(function(input, output) {
         ratesR <- ratesR %>%
             mutate(inflate.wages = adj_wage(input_wage, rates$rate, inflate=T),
                    discount.wages = adj_wage(input_wage, rates$rate, inflate=F))
-        
-        # ratesR <- tble
     })
     
     # reactive commentary
      output$inflate_txt <- renderText({
+         
          # today's year
          year.today <- c(format(Sys.Date(), "%Y"))
+         
          # get wage for the year
          wage.today <- wage.table() %>%
              filter(year == year.today) %>%
              select(inflate.wages, discount.wages)
+         
+         # text output
          c("If your wage kept up with inflation, today it would be ",
            '$', as.character(round(wage.today[1,1],2)),
            "If it hasn't kept up with inflation, your wage in 2013 terms is now ",
@@ -31,19 +33,15 @@ shinyServer(function(input, output) {
      })
     
     # assign data to output slot
-    output$time_series <- renderTable({
-        wage.table()
-    })
+    output$time_series <- renderTable({ wage.table() })
     
     # create plot in output
     output$main_plot <- renderPlotly({
         
         df <- as.data.frame(wage.table())
         
-        cat(file=stderr(), df$inflate.wages, df$rate)
-        
-        plot_ly(df, x = df$year, y = df$inflate.wages, type = 'scatter', mode = 'lines') %>%
-            add_trace(y = df$discount.wages, name = 'trace 2', mode = 'markers')
+        plot_ly(df, x = df$year, y = df$inflate.wages, name = 'wage needed', type = 'scatter', mode = 'lines') %>%
+            add_trace(y = df$discount.wages, name = 'real wage loss', mode = 'lines')
         
     })
 })
