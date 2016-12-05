84bf877b21859868ac49e12ee1f18598bade2ecc
ucg8j <lukesingham@gmail.com>
2016-12-05 11:16:52 +1100

    add link to source code in UI

--- ui.R
+++ ui.R
@@ -9,7 +9,8 @@ fluidPage(
     sidebarLayout(
         sidebarPanel(
             numericInput("wage", label = "Enter your wage", value = 75000),
-            helpText('Source: RBA.gov.au')
+            helpText('Data sourced from RBA.gov.au + treasury.gov.au'),
+            helpText(a('Code and Methodology', href='https://github.com/ucg8j/wage_inflation_calculator'))
         ),
     
     # Outputs
